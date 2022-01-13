b57a17e7d861ce78c9d81135300f8b3d65bac050
uberflip-jbooth <james.booth@uberflip.com>
2022-01-13 15:59:53 +0000

    Task 7: Styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,9 @@ import '../less/home.less';
 export default function Home() {
   return (
     <div id='home'>
-      <ImageCarousel showThumbs={false} showIndicators={true} />
+      <div className='carousel-wrapper'>
+        <ImageCarousel showThumbs={false} showIndicators={true} />
+      </div>
     </div>
   );
 }
