1bff15beafc5825665061e88b7f0846863501c60
uberflip-jbooth <james.booth@uberflip.com>
2022-01-13 14:47:58 +0000

    Task 1: Consume API and display images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import ImageCarousel from './ImageCarousel';
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <ImageCarousel />
     </div>
   );
 }
--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import axios from 'axios';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-export default function ImageCarousel() {
-  return (
-    <Carousel showThumbs={false} showIndicators={false}>
-    </Carousel>
-  );
-}
+export default class ImageCarousel extends React.Component {
+
+  // Add a constructor class to handle props and state
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: []
+    };
+  }
+
+  // When the component mounts execute the API call.
+  componentDidMount() {
+    this.loadImages();
+  }
+
+  // Load the images and update the state
+  async loadImages() {
+    const url = 'https://apodapi.herokuapp.com/search/?search_query=pluto&number=5';
+    const response = await axios.get(url);
+    if (response.status !== 200) {
+      throw new Error(`Cannot get images from ${url}`);
+    }
+
+    const images = [];
+    for (let entry of response.data) {
+      images.push({
+        url: entry.url
+      });
+    }
+
+    this.setState({images});
+  }
+
+  // Render the component. Before the component's state has any images
+  // loaded, render the LoadingSpinner component instead.
+  render() {
+    return <Carousel showThumbs={false} showIndicators={false}>
+      {
+        this.state.images.map((image, i) => {
+          return (
+            <div key={'image-carousel-' + i}>
+              <img src={image.url} alt={image.url} />
+            </div>
+          );
+        })
+      }
+    </Carousel>;
+  }
+} 
